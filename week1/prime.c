54fec46e6979130f54afb128e431e04a82b0f385
Nikhil <nikhilpant1997@gmail.com>
2014-06-29 12:22:10 -0400

    fixed prime

--- week1/prime.c
+++ week1/prime.c
@@ -6,12 +6,12 @@ float prime(int a)
 {
 	int i;
 	for (i = 2; i < (a); i++){
-		if (a % i == 0)
+		if (a % i == 0){
 			printf( "this number is not a prime\n");
-		if (a % i != 0)
-			printf("number is prime \n");
-			break;
-	}return 0;
+			return 0;
+	}
+}
+	printf("number is prime \n");
 }
 int main(int argc, const char *argv[])
 {
